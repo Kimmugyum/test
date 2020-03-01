122406300c1c7176dea26efa16385950560175b6
Kim Mugyum <gyum2726@naver.com>
2020-03-02 01:30:51 +0900

    html이름 수정 및 javascript 연결

--- script.js
+++ script.js
@@ -41,11 +41,12 @@
 
         var number = document.getElementsByClassName("number"); // 숫자에다가 클릭이벤트를 넣기
         for(var i = 0; i < number.length; i++){
+            console.log(number.length)
             number[i].addEventListener('click', clickHandler);
         }
         
-        var AC = document.getElementById("color"); // AC버튼에 reset이벤트 넣기
-            AC.addEventListener('click', reset);
+        var AC = document.getElementById("color") // AC버튼에 reset이벤트 넣기
+            AC.addEventListener('click', reset)
         
         var result2 = document.getElementById("result") // = 버튼에 클릭이벤트 넣기
             result2.addEventListener('click', result);
